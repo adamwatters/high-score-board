448ce46866de0cad2d7458eb5daf294f568579c3
Max Blum <max.blum@gmail.com>
2014-09-24 13:53:36 -0400

    Successfully storing data in local memory and returning it!

--- routes.py
+++ routes.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, url_for, request, jsonify
+from json import dumps
 
 app = Flask(__name__)
+
+default_data = \
+[             
+    {"ranking": 1, "name": "chaz", "score": 99},
+    {"ranking": 3, "name": "carl", "score": 60},
+    {"ranking": 2, "name": "mike", "score": 95} 
+]
+
 
 @app.route('/')
 def high_score():
@@ -9,8 +18,18 @@ def high_score():
 
 @app.route('/dataupdate', methods=['POST'])
 def update_data():
-    print str(jsonify(request.form))
-    return jsonify(request.form)
+    new_high_score = {}
+
+    for k in request.form.keys():
+        new_high_score[k] = request.form[k]
+
+    default_data.append(new_high_score)
+
+    return get_data()
+
+@app.route('/getdata', methods=['GET'])
+def get_data():
+    return dumps(default_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -81,7 +81,7 @@ var App = React.createClass({
 	      type: 'POST',
 	      data: entry,
 	      success: function(data) {
-	        this.setState({data: [data]});
+	        this.setState({data: data});
 	      }.bind(this),
 	      error: function(xhr, status, err) {
 	        console.error(this.props.url, status, err.toString());
@@ -92,7 +92,7 @@ var App = React.createClass({
 
 	componentDidMount: function() {
 	    $.ajax({
-	      url: this.props.url,
+	      url: 'getdata',
 	      dataType: 'json',
 	      success: function(data) {
 	        this.setState({data: data});
